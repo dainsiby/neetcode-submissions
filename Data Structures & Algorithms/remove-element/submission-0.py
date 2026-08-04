68039d87b27741354870f052e8ffe5ea19d4aaf7
dainsiby <dainsiby14@gmail.com>
2026-08-04 10:01:28 +0530

    Add: remove-element - submission-0

---
+++ Data Structures & Algorithms/remove-element/submission-0.py
@@ -0,0 +1,8 @@
+class Solution:
+    def removeElement(self, nums: List[int], val: int) -> int:
+        for i in nums[:]:
+            if i==val:
+                nums.remove(val)
+        k=len(nums)
+        return k        
+        
